459f9b0b295dd0e0dc89f6d58a241f39b8be4ec2
yourpallavikkaawdegit config --global user.password your <pallavi.jagtap512@gmail.com>
2021-09-24 11:02:17 +0530

    Added:refactored code day4

--- src/main/java/com/brigdelabs/quantitymeasurement/Length.java
+++ src/main/java/com/brigdelabs/quantitymeasurement/Length.java
@@ -4,71 +4,24 @@ import jdk.jshell.execution.Util;
 
 import java.util.Objects;
 public class Length {
-
-    private static final double FEET_TO_INCH = 12;
-    private static final double FEET_TO_YARD = 3;
-    private static final double INCH_TO_YARD = 36;
-    private static final double INCH_TO_CENTIMETER = 2.54;
-    private static final double GALLON_TO_LITRES = 3.78;
-    private static final double LITRES_TO_MILLILITER = 1000;
-    private static final double KILOGRAM_TO_GRAM = 1000;
-    private static final double TONNE_TO_KILOGRAM = 1000;
-    private static final double FAHRENHEIT_TO_CELSIUS = 32;
     private final double value;
     private final Unit unit;
-
-
-    enum Unit {FEET, INCH, YARD, CENTIMETER, GALLON, LITRES, MILLILITER, GRAM, TONNE, KILOGRAM, FAHRENHEIT, CELSIUS};
 
     public Length(Unit unit, double value) {
         this.unit = unit;
         this.value = value;
     }
 
-    public boolean compare(Length value2) {
-        if (this.unit.equals(Unit.FEET) && value2.unit.equals(Unit.FEET))
-            return Double.compare(this.value, value2.value) == 0;
-        if (this.unit.equals(Unit.FEET) && value2.unit.equals(Unit.INCH))
-            return Double.compare(this.value * FEET_TO_INCH, value2.value) == 0;
-        if (this.unit.equals(Unit.FEET) && value2.unit.equals(Unit.YARD))
-            return Double.compare(this.value, value2.value * FEET_TO_YARD) == 0;
-        if (this.unit.equals(Unit.INCH) && value2.unit.equals(Unit.YARD))
-            return Double.compare(this.value, value2.value * INCH_TO_YARD) == 0;
-        if (this.unit.equals(Unit.INCH) && value2.unit.equals(Unit.CENTIMETER))
-            return Double.compare(this.value * INCH_TO_CENTIMETER, value2.value) == 0;
-        if (this.unit.equals(Unit.GALLON) && value2.unit.equals(Unit.LITRES))
-            return Double.compare(this.value * GALLON_TO_LITRES, value2.value) == 0;
-        if (this.unit.equals(Unit.LITRES) && value2.unit.equals(Unit.MILLILITER))
-            return Double.compare(this.value * LITRES_TO_MILLILITER, value2.value) == 0;
-        if (this.unit.equals(Unit.KILOGRAM) && value2.unit.equals(Unit.GRAM))
-            return Double.compare(this.value * KILOGRAM_TO_GRAM, value2.value) == 0;
-        if (this.unit.equals(Unit.TONNE) && value2.unit.equals(Unit.KILOGRAM))
-            return Double.compare(this.value * TONNE_TO_KILOGRAM, value2.value) == 0;
-        if (this.unit.equals(Unit.FAHRENHEIT) && value2.unit.equals(Unit.CELSIUS))
-            return Double.compare(((this.value - FAHRENHEIT_TO_CELSIUS) / 1.8), value2.value) == 0;
-        return false;
+    public Double unitConversion(double value, Unit temperature) {
+        return Unit.celTofah(value, temperature);
     }
 
-    public double addLengthsToInches(Length inch){
-        if(this.unit.equals(Unit.INCH) && inch.unit.equals(Unit.INCH))
-            return this.value + inch.value;
-        if(this.unit.equals(Unit.FEET) && inch.unit.equals(Unit.INCH))
-            return (this.value * FEET_TO_INCH) + inch.value;
-        if(this.unit.equals(Unit.FEET) && inch.unit.equals(Unit.FEET))
-            return (this.value * FEET_TO_INCH) + (inch.value * FEET_TO_INCH);
-        if(this.unit.equals(Unit.INCH) && inch.unit.equals(Unit.CENTIMETER))
-            return (this.value) + (inch.value / INCH_TO_CENTIMETER);
-        return 0;
+    public boolean compare(Length l2) {
+        return Unit.compare(this.value, this.unit, l2.value, l2.unit);
     }
 
-    public double addVolumesToLitres(Length litres) {
-        if(this.unit.equals(Unit.GALLON) && litres.unit.equals(Unit.LITRES))
-            return (this.value * GALLON_TO_LITRES) + litres.value;
-        if(this.unit.equals(Unit.LITRES) && litres.unit.equals(Unit.MILLILITER))
-            return (this.value * LITRES_TO_MILLILITER) + litres.value;
-        if(this.unit.equals(Unit.TONNE) && litres.unit.equals(Unit.GRAM))
-            return (this.value * TONNE_TO_KILOGRAM) + (litres.value / KILOGRAM_TO_GRAM);
-        return 0;
+    public double add(Length l2) {
+        return Unit.add(this.value, this.unit, l2.value, l2.unit);
     }
 
     @Override
